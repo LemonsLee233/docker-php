819abaac101431aa31adbe3624a2e6fe5a5b968f
lemonslee <649423722@qq.com>
2025-08-06 17:04:54 +0800

    add file

--- src/database.php
+++ src/database.php
@@ -4,13 +4,14 @@
 $db_host = getenv('DB_HOST');
 $db_name = getenv('DB_NAME');
 $db_user = getenv('DB_USER');
+echo $db_host,"<br>",$db_name,"<br>",$db_user,"<br>";
 
 // Read the password file path from an environment variable
 $password_file_path = getenv('PASSWORD_FILE_PATH');
-
+echo $password_file_path,"<br>";
 // Read the password from the file
 $db_pass = trim(file_get_contents($password_file_path));
-
+echo $db_pass,"<br>",'test', "<br>";
 // Create a new PDO instance
 $db_handle = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 
